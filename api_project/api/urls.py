7e71203d591a83eabde245f4bad75ccca0d29729
Bayisa Daba <bayisa615@gmail.com>
2025-03-06 10:09:30 +0300

    modify

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from. views import BookList
 
 urlpatterns = [
-    path('books/',BookList.as_view(), name='book_list'),
+    path('books/',BookList.as_view(), name='book-list'),
 ]
